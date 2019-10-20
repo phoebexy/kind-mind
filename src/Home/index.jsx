261a43daa65ebab77431c6c9c0a1b7f1e988ebb6
Phoebe Yao <phoebeyao2@gmail.com>
2019-10-19 18:47:41 -0700

    more home page changes

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,10 +1,11 @@
 import React, { Fragment, Component } from "react";
+import "./Home.module.scss";
 
 class Home extends Component {
   render() {
     return (
       <Fragment>
-        <h1>Hi Rubens</h1>
+        <h1 className="title-1">Kind Mind</h1>
         <h1>Hi Will</h1>
         <h1>Hi Phoebe</h1>
       </Fragment>
