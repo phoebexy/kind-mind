a812be4583cbaf4eaa5fc38f7698bffe1b341d5d
Rubens Jean-Paul <rubens.jeanpaul@gmail.com>
2019-10-19 19:45:04 -0700

    Scss can now be compiled to css

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,10 +1,11 @@
 import React, { Fragment, Component } from "react";
+import "./Home.scss";
 
 class Home extends Component {
   render() {
     return (
       <Fragment>
-        <h1>Hi Rubens</h1>
+        <h1 className="diff">Hi Rubens</h1>
         <h1>Hi Will</h1>
         <h1>Hi Phoebe</h1>
       </Fragment>
