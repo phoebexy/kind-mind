2b66c6dad6ab8dc704fddba107d694fe9aea9147
Phoebe Yao <phoebeyao2@gmail.com>
2019-10-19 17:09:15 -0700

    home page init

---
+++ src/Home/index.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment, Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hi Rubens</h1>
+        <h1>Hi Will</h1>
+        <h1>Hi Phoebe</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default Home;
