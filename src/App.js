b93e9508f7d0d0ba59d5300c426609ccaeec3790
unknown <willells@stanford.edu>
2019-10-20 13:45:52 -0700

    new stuf

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
 import './App.css';
 import Home from './Home'
+import Message from './Message'
 
 function App() {
   return (
     <Fragment>
-      <Home />
+      <Message />
     </Fragment>
   );
 }
